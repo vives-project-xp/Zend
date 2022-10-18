1d1f988332a9a56eb03d3851cea781a80a8f8976
Crulzor <bartcrul@hotmail.com>
2022-10-19 01:37:14 +0200

    changed function class a bit - added spiral

--- pi_code/python/functions.py
+++ pi_code/python/functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 
@@ -6,23 +7,40 @@ class Functions:
     def __init__(self):
         self._x = []
         self._y = []
-        self._n = np.linspace(0, 2*np.pi, 100)
+        self._intX = []
+        self._intY =[]
+        self._n = np.linspace(0, 8*np.pi, 100)
     
     def testFigure(self):
-        tempX = np.sin(self._n)**3
-        tempY = np.cos(self._n)**3
-        roundX = np.around(tempX, 4) *100
-        roundY = np.around(tempY, 4) * 100
-        intX = roundX.astype(int)
-        intY = roundY.astype(int)
-        self._x = intX.astype(str)
-        self._y = intY.astype(str)
+        x = np.sin(self._n)**3
+        y = np.cos(self._n)**3
+        roundX = np.around(x, 3) *100
+        roundY = np.around(y, 3) * 100
+        self._intX = roundX.astype(int)
+        self._intY = roundY.astype(int)
+        self._x = self._intX.astype(str)
+        self._y = self._intY.astype(str)
+        
+    def spiral(self):
+        x = self._n*(np.cos(self._n))
+        y = self._n*(np.sin(self._n))
+        tempX = np.around(x, 3) * 10
+        tempY = np.around(y, 3) * 10
+        self._intX = tempX.astype(int)
+        self._intY = tempY.astype(int)
+        self._x = self._intX.astype(str)
+        self._y = self._intY.astype(str)
+
+    def clearFunction(self):
+        return 0
+
+    def plotFigure(self):
+        plt.scatter(self._intX, self._intY)
+        plt.show()
 
 
     def getX(self):
         return self._x
     def getY(self):
         return self._y
-
-
 
