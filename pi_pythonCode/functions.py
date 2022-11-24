0a166715f9e35e76c3a89c5eba855f7c443a377f
Crulzor <bartcrul@hotmail.com>
2022-11-24 14:29:31 +0100

    small update

--- pi_pythonCode/functions.py
+++ pi_pythonCode/functions.py
@@ -1,55 +1,26 @@
+from cmath import cos, pi, sin, sqrt
 import numpy as np
-from scipy import signal #niet vergeten te installeren op rPi
 import matplotlib.pyplot as plt
-
-
 
 class Functions:
     def __init__(self):
-        self._x = []
-        self._y = []
+        self._xFunction = []
+        self._yFunction = []
+
+        self._n = np.linspace(0, 2*np.pi,100)
+
+        self.defaultOffsetX = 100
+        self.defaultOffsetY = 100
+        self.defaultOffsetMin = 0
+        self.defaultOffsetMax = 200
+        self.defaultRoundDown = 3
+        self.defaultMultiplier = 5
+        self.defaultGrowth = 1.1
+
+        self._strX = []
+        self._strY = []
         self._intX = []
         self._intY =[]
-        self._n = np.linspace(0, 8*np.pi, 500)
-    
-    def testFigure(self):
-        x = np.sin(self._n)**3
-        y = np.cos(self._n)**3
-        roundX = np.around(x, 3) *100
-        roundY = np.around(y, 3) * 100
-        self._intX = roundX.astype(int)
-        self._intY = roundY.astype(int)
-        self._x = self._intX.astype(str)
-        self._y = self._intY.astype(str)
-        
-    def spiral(self):
-        x = self._n*(np.cos(self._n))
-        y = self._n*(np.sin(self._n))
-        roundX = abs(np.around(x, 3) * 10)
-        roundY = abs(np.around(y, 3) * 10)
-        self._intX = roundX.astype(int)
-        self._intY = roundY.astype(int)
-        self._x = self._intX.astype(str)
-        self._y = self._intY.astype(str)
-
-    def clearFunction(self):                #voorlopig een rampfunctie
-        y = signal.sawtooth(3*self._n)
-        roundY = np.around(y, 3) * 10
-        roundX = np.around(self._n, 3) * 10
-        self._intX = roundX.astype(int)
-        self._intY = roundY.astype(int)
-        self._x = self._intX.astype(str)
-        self._y = self._intY.astype(str)
-    
-    def drawPolygon(self, amount):
-        n = np.linspace(0, 2*np.pi, amount + 1)
-        self._xFunction = [np.cos(n)]
-        self._yFunction = [np.sin(n)]
-        self.parser()
-
-    def plotFigure(self):
-        plt.scatter(self._intX, self._intY)
-        plt.show()
 
     def parser(self):
         x = np.around(self._xFunction, self.defaultRoundDown) * self.defaultMultiplier + self.defaultOffsetX
@@ -58,18 +29,50 @@ class Functions:
         self._intY = y.astype(int)
         self._strX = self._intX.astype(str)
         self._strY = self._intY.astype(str)
-    
-    def GetRadius(x1,x2,y1,y2):
-        return np.sqrt((x2 - x1)**2+ (y2 - y1)**2)
 
+    def drawStar(self, amount, angle = 0):
+        self.drawPolygon(amount, angle)
+
+    def drawCoil(self, amount):
+        n = np.linspace(0, 2*np.pi, amount * 50)
+        self._xFunction = [(np.cos((amount + 1)*n) - np.cos(n)) / amount]
+        self._yFunction = [(np.sin((amount + 1)*n) - np.sin(n)) / amount]
+        self.parser()
+
+    def drawPolygon(self, amount = 3, angle = 0):
+        angle = (angle * np.pi / 180)
+        n = np.linspace(angle, 2*np.pi + angle, amount + 1)
+        self._xFunction = [np.cos(n)]
+        self._yFunction = [np.sin(n)]
+        self.parser()
+
+    def spiral(self):
+        n = np.linspace(0, 4* np.pi, 50)
+        self._xFunction = [ np.sqrt(n/20)*(np.cos(n))/10]
+        self._yFunction = [ np.sqrt(n/20)*(np.sin(n))/10]
+        n = np.linspace(4* np.pi, 8* np.pi, 250)
+        self._xFunction = [ np.sqrt(n/20)*(np.cos(n))/10]
+        self._yFunction = [ np.sqrt(n/20)*(np.sin(n))/10]
+    
+        self.parser()
+
+    def clearFunction(self):
+        return 0
+
+    def plotFigure(self):
+        plt.scatter(self._intX, self._intY)
+        plt.show()
 
     def getX(self):
         return self._x
     def getY(self):
         return self._y
 
-    def getIntX(self):
-        return self._intX
-    def getIntY(self):
-        return self._intY
 
+
+test = Functions()
+test.drawPolygon(4)
+test.plotFigure()
+
+test.drawPolygon(4, 45)
+test.plotFigure()
